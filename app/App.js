3a0e70b1f9407cbc926f39619da3525cf543cb55
sergsal <sergio.saldarriaga88@gmail.com>
2016-11-11 19:06:58 -0500

    Adds file structure and base index and app files

---
+++ app/App.js
@@ -0,0 +1,13 @@
+// Main React Dependencies
+const React = require('react');
+const ReactDOM = require ('react-dom');
+
+// Main Component
+const Main = require('./Components/Main');
+
+// Renders "Main")
+ReactDOM.render(
+
+	<Main />,
+	document.getElementById('app')
+)
---
+++ app/Components/utils/helpers.js
@@ -0,0 +1,6 @@
+// Include the axios package for performing HTTP requests (promise based alternative to request)
+var axios = require('axios');
+
+
+// We export the helpers function (which contains getGithubInfo)
+module.exports = helpers;
